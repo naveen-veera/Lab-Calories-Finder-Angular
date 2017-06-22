7b510ad506c679f89d837d736bbb05900213cfef
Nicholas Borbe <nicholasborbe@Nicholass-MacBook-Pro-2.local>
2017-06-22 19:47:10 -0400

    solution added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodSearchPipe } from './food-search.pipe';
+import { MyFoodsComponent } from './my-foods/my-foods.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodListComponent,
+    FoodSearchPipe,
+    MyFoodsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
